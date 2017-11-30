d2318c493638354fa49a8549d62fd9f5eee9886e
kirill <myemail@mail.ru>
2017-11-30 08:15:27 +0300

    'html

---
+++ shulga/My_first_project/scripts/script.js
@@ -0,0 +1,8 @@
+compressed.onclick = function() {
+  this.style.letterSpacing = '0px';
+  this.style.fontWeight = 'normal';
+  this.style.width = 'auto';
+  this.style.height = 'auto';
+  this.style.backgroundColor = 'transparent';
+  this.style.color = 'black';
+}
