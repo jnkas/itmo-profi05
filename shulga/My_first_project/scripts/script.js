a01d27aaf49d24d605211aff5cd8940daf70b0d3
kirill <myemail@mail.ru>
2017-12-07 07:49:42 +0300

    'Laboratornaya'

--- shulga/My_first_project/scripts/script.js
+++
@@ -1,8 +0,0 @@
-compressed.onclick = function() {
-  this.style.letterSpacing = '0px';
-  this.style.fontWeight = 'normal';
-  this.style.width = 'auto';
-  this.style.height = 'auto';
-  this.style.backgroundColor = 'transparent';
-  this.style.color = 'black';
-}
