9a7d0d8a6a051136f6c6a6d1f7ff1e9ba0535d18
OlegDubrovskiy <don-it@bk.ru>
2017-12-14 22:03:14 +0300

    add js

---
+++ dubrovskiy/JavaScript/js/script.js
@@ -0,0 +1,34 @@
+// однострочный комментарий
+/*
+мнолострочный комментарий
+*/
+// alert("код выполнился из файла");
+
+console.log(4 + 1);
+var a = 10;
+// var a = a - 5;
+// console.log(a);
+
+// if (4 > 1  || 8 > 10) {
+// if (a!==5) {
+// 	console.log("верно");
+// 	} else {
+// 		console.log("не верно");
+// 	}
+var three = 3,
+	treeStr = "3";
+console.log(three === treeStr);
+
+// var login = prompt("Ваш логин");
+// if (login == "admin") {
+// 	var	pass = prompt("Ваш пароль");
+// 	if (pass == "пароль") {
+// 		alert("Go!");
+// 	} else {
+// 		alert("No access :");
+// } else {
+// 	alert("не  найден");
+// }
+a = prompt("первое число");
+b = prompt("второе число");
+с = prompt("третье число");
