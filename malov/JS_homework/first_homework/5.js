be80312d3ac20b3f72847a25e8c02e6ce12ca8e6
Alex <allmalov@mail.ru>
2017-12-18 23:23:25 +0300

    JS_1_homework

--- malov/JS_homework/first_homework/5.js
+++ malov/JS_homework/first_homework/5.js
@@ -2,7 +2,7 @@ var e = prompt("Введите 1 число");
 var f = prompt("Введите 2  число");
 var g = prompt("Введите 3 число");
 
-if(e===f || e===g){
+if(e===f || e===g || f===g){
     alert("true");
 }else{
     alert("false");
