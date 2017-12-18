29b10c05f35b8bdb670623dbffbfa2f85f64551b
Alex <allmalov@mail.ru>
2017-12-18 23:13:13 +0300

    JS_first_homework

---
+++ malov/JS_homework/first_homework/3.js
@@ -0,0 +1,24 @@
+var d = prompt("Введите число в диапазоне от 0-9")
+if(d==0){
+    alert("Ноль");
+}else if(d==1){
+    alert("Один");
+}else if(d==2){
+    alert("Два");
+}else if(d==3){
+    alert("Три");
+}else if(d==4){
+    alert("Четыре");
+}else if(d==5){
+    alert("Пять");
+}else if(d==6){
+    alert("Шесть");
+}else if(d==7){
+    alert("Семь");
+}else if(d==8){
+    alert("Восемь");
+}else if(d==9){
+    alert("Девять");
+}else{
+    alert("Неверное число");
+}
---
+++ malov/JS_homework/first_homework/5.js
@@ -0,0 +1,9 @@
+var e = prompt("Введите 1 число");
+var f = prompt("Введите 2  число");
+var g = prompt("Введите 3 число");
+
+if(e===f || e===g){
+    alert("true");
+}else{
+    alert("false");
+}
